f7b5d79565b9967bb1e986f6c1a8e4e3946e50b0
brijshah <brijshah0@gmail.com>
2018-08-09 18:51:19 -0700

    path to ansible top level play

--- run.sh
+++ run.sh
@@ -8,6 +8,6 @@ exit-failure() {
 BOOTSTRAP=$1
 DEPLOY=$2
 
-which pip2 || apt-get install -y python-pip || exit-failure "pip failed to install."
+which pip2 || apt-get update;apt-get install -y python-pip || exit-failure "pip failed to install."
 which ansible || pip install ansible || exit-failure "ansible failed to install."
-ansible-playbook -i localhost deploy_todo_app.yml -e "bootstrap=$BOOTSTRAP deploy=$DEPLOY" --become || exit-failure "playbook failed to complete successfuly."
+ansible-playbook -i localhost ./ansible/deploy_todo_app.yml -e "bootstrap=$BOOTSTRAP deploy=$DEPLOY" --become || exit-failure "playbook failed to complete successfuly."
